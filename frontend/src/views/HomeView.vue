c56208350586ed5f84601b3640a22d1aefc0c585
Ruben Gonzalez <rubengaviles@gmail.com>
2022-03-24 16:56:36 +0100

    improved homepage elements

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="home">
     <div class="grid grid-cols-12 gap-1">
-      <div id="avatar" class="col-span-3 text-white m-2 p-1 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl" style="backdrop-filter: blur(20px);">Avatar<br />{{ sentiment }}</div>
-      <div id="stream" class="col-span-6 text-white m-2 p-1 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">        <video width="320" height="240" controls @playing="startPlaying" @pause="updatePaused">
+      <div id="avatar" class="col-span-3 text-white m-2 p-3 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl" style="backdrop-filter: blur(20px);">AUDITAR<br /><!--{{ sentiment }}-->
+      <img class="flex items-center mx-auto mt-10 filter drop-shadow-2xl" src="../assets/happy.png" alt="Happy Avatar" width="200">
+      </div>
+      <div id="stream" class="col-span-6 text-white m-2 p-3 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">
+        <div class="mb-2">KRISTOFERS STREAM</div>
+        <video class="flex items-center mx-auto rounded-lg" width="550" controls @playing="startPlaying" @pause="updatePaused">
           <source src="http://localhost:8080/video/" type="video/mp4" />
           <track kind="captions" />
           Your browser does not support the video tag.
         </video>
 </div>
-      <div id="chat" class="col-span-3 content-end text-white m-2 p-1 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">
+      <div id="chat" class="flex items-end mx-auto col-span-3 text-white m-2 p-3 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">
         <div v-for="message in chat" :key="message.timestamp">
           <span class="text-blue-400">{{ message.username }}&nbsp;</span>
           <span> {{ message.message }}</span>
         </div>
         <input
           v-model="messageBox"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          class="content-end shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
         <button
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
@@ -27,8 +31,8 @@
       </div>
     </div>
     <div class="grid grid-cols-12 gap-1">
-      <div id="stream" class="col-span-7 text-white m-2 p-1 rounded-lg font-mono h-56 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">Line thingy</div>
-      <div id="stream" class="col-span-5 text-white m-2 p-1 rounded-lg font-mono h-56 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl"></div>
+      <div id="stream" class="col-span-7 text-white m-2 p-3 rounded-lg font-mono h-56 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">Line thingy</div>
+      <div id="stream" class="col-span-5 text-white m-2 p-3 rounded-lg font-mono h-56 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl"></div>
     </div>
   </div>
 </template>
