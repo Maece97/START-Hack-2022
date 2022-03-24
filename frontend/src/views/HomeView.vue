4cde64cf8cda94ca079ff7361e799493b22ad992
Ruben Gonzalez <rubengaviles@gmail.com>
2022-03-24 15:48:29 +0100

    homepage initial design

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
     <div class="grid grid-cols-12 gap-1">
-      <div id="avatar" class="col-span-3">Avatar<br />{{ sentiment }}</div>
-      <div id="stream" class="col-span-6">Stream</div>
-      <div id="chat" class="col-span-3">
+      <div id="avatar" class="col-span-3 text-white m-2 p-1 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl" style="backdrop-filter: blur(20px);">Avatar<br />{{ sentiment }}</div>
+      <div id="stream" class="col-span-6 text-white m-2 p-1 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">Stream</div>
+      <div id="chat" class="col-span-3 content-end text-white m-2 p-1 rounded-lg font-mono h-96 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">
         <div v-for="message in chat" :key="message.timestamp">
           <span class="text-blue-400">{{ message.username }}&nbsp;</span>
           <span> {{ message.message }}</span>
@@ -13,7 +13,7 @@
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
         <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
           @click="sendMessage()"
           :disabled="messageBox.length === 0"
         >
@@ -22,8 +22,8 @@
       </div>
     </div>
     <div class="grid grid-cols-12 gap-1">
-      <div id="stream" class="col-span-7">Line thingy</div>
-      <div id="stream" class="col-span-5"></div>
+      <div id="stream" class="col-span-7 text-white m-2 p-1 rounded-lg font-mono h-56 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl">Line thingy</div>
+      <div id="stream" class="col-span-5 text-white m-2 p-1 rounded-lg font-mono h-56 bg-white shadow-lg bg-clip-padding bg-opacity-10 border border-gray-200 backdrop-filter backdrop-blur-xl"></div>
     </div>
   </div>
 </template>
@@ -67,12 +67,12 @@ export default defineComponent({
 
 <style scoped>
 #avatar {
-  border: solid 1px black;
+  border: solid 1px white;
 }
 #stream {
-  border: solid 1px black;
+  border: solid 1px white;
 }
 #chat {
-  border: solid 1px black;
+  border: solid 1px white;
 }
 </style>
