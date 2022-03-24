9fe0b7dd39223cfbad31435ec2aea604abb4538f
Marcel <marcel@maece.ch>
2022-03-24 12:36:25 +0100

    frontend chat funcrtion

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'import/no-unresolved': 'off',
     'import/extensions': 'off',
+    'max-len': 'off',
   },
 };
